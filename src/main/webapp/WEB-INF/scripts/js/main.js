4909504a138290b828f5b2d7aabadd1ea06b36e6
Jad Bouchouka <jad.bouchouka@sogeti.lu>
2016-03-22 11:43:01 +0100

    fix : display countries in a table

--- src/main/webapp/WEB-INF/scripts/js/main.js
+++ src/main/webapp/WEB-INF/scripts/js/main.js
@@ -1,42 +1,75 @@
 (function() {
 
-	var app = angular.module('countriesModule', [])
-					 .controller('CountryListCtrl', ['$scope', '$http', CountryListCtrl]);
+	var app = angular.module('countriesModule', []);
 
-	function CountryListCtrl($scope, $http) {
-
-		//this.countries = {};
-
-		$http.get('http://localhost:8060/spring-activiti-tutos/getAllCountries').success(
-				function(data) {
-					$scope.countries = data;
-				});
-	}
-	
-	app.directive('countryTable', function() {
+	//Directives
+	app.directive('countryTable', function($compile) {
 		return {
 			restrict : 'E',
 			link : function(scope, element, attrs) {
 
-				var html = '<table>';
+				scope.$watch('countries', function() {
+					element.html(scope.countries);
+					$compile(element.contents())(scope);
+				});
 
-				angular.forEach(scope[attrs.elements], function(row, index) {
-					if (index % 3 == 0) {
-						html += '<tr>';
-					}
-
-					html += '<td>' + row.name + '</td>';
-
-					if ((index + 1) % 3 == 0) {
-						html += '</tr>';
-					}
-				})
-
-				html += '</table>';
-
-				element.replaceWith(html);
 			}
 		}
+	});
+
+	//Services
+	app.factory('httpGet', function($http) {
+						return {
+							get : function() {
+								return $http.get('http://localhost:8060/spring-activiti-tutos/getAllCountries')
+										// this will return a promise to controller
+										.then(
+												function(response) {
+													if (typeof response.data === 'object') {
+														return response.data;
+													} else {
+														// invalid response
+														return $q.reject(response.data);
+													}
+
+												},
+												function(response) {
+													// something went wrong
+													return $q.reject(response.data);
+												});
+							}
+						}
+					});
+
+	//Controllers
+	app.controller('CountryListCtrl', function($scope, httpGet) {
+
+		httpGet.get().then(function(data) {
+			// promise fulfilled
+
+			var html = '<table>';
+			angular.forEach(data, function(row, index) {
+				if (index % 3 == 0) {
+					html += '<tr>';
+				}
+
+				html += '<td>' + row.name + '</td>';
+
+				if ((index + 1) % 3 == 0) {
+					html += '</tr>';
+				}
+			})
+
+			html += '</table>';
+
+			$scope.countries = html;
+
+		}, function(error) {
+			// promise rejected, could log the error with: console.log('error',
+			// error);
+			console.log("Damn it");
+		});
+
 	});
 
 })();
--- src/main/webapp/WEB-INF/views/listCountries.jsp
+++ src/main/webapp/WEB-INF/views/listCountries.jsp
@@ -1,24 +1,15 @@
 <!doctype html>
 <html ng-app="countriesModule">
-	<head>
-		<title>Hello AngularJS</title>
-		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"></script>
-    	<script type="text/javascript"><%@include file="../scripts/js/main.js" %></script>
-	</head>
+<head>
+<title>Hello AngularJS</title>
+<script
+	src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"></script>
+<script type="text/javascript"><%@include file="../scripts/js/main.js" %></script>
+</head>
 
-	<body ng-controller="CountryListCtrl">
-	
-	<country-table elements="countries"></country-table>
-	
-	<!-- <table>
-		<tr>
-			<td ng-repeat="country in countries">
-				<p>The country is {{country.name}}</p>
-				<p>The capital is {{country.capital}}</p>
-				<p>People who go there : {{country.visitors.length}}</p>
-			</td>
-		</tr>		
-	</table> -->
-		
-	</body>
+<body ng-controller="CountryListCtrl">
+
+	<country-table rows="countries"></country-table>
+
+</body>
 </html>
